292116f1d9e5f850286ab01f0744a13ce401a5c0
ankur.sheel@gmail.com <ankur.sheel@gmail.com@c580c124-fe00-11dd-b556-9305e745af9e>
2012-11-27 12:16:43 +0000

    Updated by AS moving to directx 11(WIP) 1) removed some compile errors from Game and gameflowstates
    git-svn-id: https://sppong.googlecode.com/svn/trunk@250 c580c124-fe00-11dd-b556-9305e745af9e

--- Source/Game/src/Game/GameFlowStates.cpp
+++ Source/Game/src/Game/GameFlowStates.cpp
@@ -16,16 +16,16 @@
 #include "Elements/Score.h"
 #include "Elements/Ball.h"
 #include "Sprite.hxx"
-#include "CollisionChecker.hxx"
+//#include "CollisionChecker.hxx"
 #include "Sound.hxx"
 #include "MPongView.h"
 #include "myString.h"
 #include "Timer.hxx"
-#include "Font.hxx"
 #include "BaseControl.hxx"
 #include "MessageDispatchManager.hxx"
 #include "FSM\Telegram.h"
 #include "Color.h"
+#include "FontManager.hxx"
 
 using namespace MySound;
 using namespace Graphics;
@@ -52,12 +52,17 @@ cStateTitleScreen* cStateTitleScreen::Instance()
 
 void cStateTitleScreen::VOnEnter(cGame *pGame)
 {
-	if (pGame->m_pHumanView->m_pParentControl != NULL)
+	if (pGame->m_pHumanView->m_pAppWindowControl != NULL)
 	{
-		IBaseControl * pLabelControl = IBaseControl::CreateLabelControl(300, 60, 400, true, DEFAULT_CHARSET
-			, "JokerMan", DT_CENTER, cColor::RED.GetColor(), "MPONG");
-		pGame->m_pHumanView->m_pParentControl->VAddChildControl(pLabelControl);
-		pLabelControl->VSetPosition(cVector3(pGame->m_iDisplayWidth/4, 0, 0.f));
+		stLabelControlDef def;
+		def.pFont = IFontManager::GetInstance()->VGetFont("arial.fnt"); // JokerMan
+		def.textColor = cColor::RED;
+		def.strText = "MPONG";
+		def.fTextHeight = 300;
+
+		IBaseControl * pLabelControl = IBaseControl::CreateLabelControl(def);
+		pGame->m_pHumanView->m_pAppWindowControl->VAddChildControl(shared_ptr<IBaseControl>(pLabelControl));
+		pLabelControl->VSetPosition(cVector2(pGame->m_iDisplayWidth/4, 0));
 	}
 	IMessageDispatchManager::GetInstance()->VDispatchMessage(2.0f, pGame->VGetID(), pGame->VGetID(), MSG_SHOWMENU, NULL);
 }
@@ -103,33 +108,40 @@ cStateMenuScreen* cStateMenuScreen::Instance()
 
 void cStateMenuScreen::VOnEnter(cGame *pGame)
 {
-	if (pGame->m_pHumanView->m_pParentControl != NULL)
+	if (pGame->m_pHumanView->m_pAppWindowControl != NULL)
 	{
-		IBaseControl * pSinglePlayerButton = IBaseControl::CreateButtonControl("Sprites\\buttonDefault.png"
-			, "Sprites\\buttonPressed.png", "Single Player ", 50, 25, 400, true, DEFAULT_CHARSET
-			, "Vladimir Script", DT_CENTER, cColor::BLUE.GetColor(), true);
-		pGame->m_pHumanView->m_pParentControl->VAddChildControl(pSinglePlayerButton);
-		pSinglePlayerButton->VSetPosition(cVector3(311.f, 310.f, 0.f));
+		stButtonControlDef buttonDef;
+		buttonDef.bAutoSize = true;
+		buttonDef.strDefaultImage = "Sprites\\buttonDefault.png";
+		buttonDef.strPressedImage = "Sprites\\buttonPressed.png";
+		buttonDef.labelControlDef.pFont = IFontManager::GetInstance()->VGetFont("arial.fnt"); // Vladimir Script
+		buttonDef.labelControlDef.strText = "Single Player";
+		buttonDef.labelControlDef.textColor = cColor::BLUE;
+		buttonDef.labelControlDef.fTextHeight = 50;
+
+		IBaseControl * pSinglePlayerButton = IBaseControl::CreateButtonControl(buttonDef);
+		pGame->m_pHumanView->m_pAppWindowControl->VAddChildControl(shared_ptr<IBaseControl>(pSinglePlayerButton));
+		pSinglePlayerButton->VSetPosition(cVector2(311.f, 310.f));
 		function<void (bool)> callbackSinglePlayerBtn;
 		callbackSinglePlayerBtn = bind(&cGame::SinglePlayerButtonPressed, pGame, _1);
 		pSinglePlayerButton->VRegisterCallBack(callbackSinglePlayerBtn);
 
-		IBaseControl * pMultiPlayerButton = IBaseControl::CreateButtonControl("Sprites\\buttonDefault.png"
-			, "Sprites\\buttonPressed.png", "MultiPlayer", 50, 25, 400, true, DEFAULT_CHARSET
-			, "Vladimir Script", DT_CENTER, cColor::BLUE.GetColor(), true);
-		pGame->m_pHumanView->m_pParentControl->VAddChildControl(pMultiPlayerButton);
-		pMultiPlayerButton->VSetPosition(cVector3(311.f, 366.f, 0.f));
-		pMultiPlayerButton->VSetSize(pSinglePlayerButton->VGetWidth(), pSinglePlayerButton->VGetHeight());
+		buttonDef.labelControlDef.strText = "MultiPlayer";
+		
+		IBaseControl * pMultiPlayerButton = IBaseControl::CreateButtonControl(buttonDef);
+		pGame->m_pHumanView->m_pAppWindowControl->VAddChildControl(shared_ptr<IBaseControl>(pMultiPlayerButton));
+		pMultiPlayerButton->VSetPosition(cVector2(311.f, 366.f));
+		pMultiPlayerButton->VSetSize(cVector2(pSinglePlayerButton->VGetWidth(), pSinglePlayerButton->VGetHeight()));
 		function<void (bool)> callbackMultiPlayerBtn;
 		callbackMultiPlayerBtn = bind(&cGame::MultiPlayerButtonPressed, pGame, _1);
 		pMultiPlayerButton->VRegisterCallBack(callbackMultiPlayerBtn);
 
-		IBaseControl * pQuitButton = IBaseControl::CreateButtonControl("Sprites\\buttonDefault.png"
-			, "Sprites\\buttonPressed.png", "Quit", 50, 25, 400, true, DEFAULT_CHARSET
-			, "Vladimir Script", DT_CENTER, cColor::BLUE.GetColor(), true);
-		pGame->m_pHumanView->m_pParentControl->VAddChildControl(pQuitButton);
-		pQuitButton->VSetPosition(cVector3(311.f, 422.f, 0.f));
-		pQuitButton->VSetSize(pSinglePlayerButton->VGetWidth(), pSinglePlayerButton->VGetHeight());
+		buttonDef.labelControlDef.strText = "Quit";
+		
+		IBaseControl * pQuitButton = IBaseControl::CreateButtonControl(buttonDef);
+		pGame->m_pHumanView->m_pAppWindowControl->VAddChildControl(shared_ptr<IBaseControl>(pQuitButton));
+		pQuitButton->VSetPosition(cVector2(311.f, 422.f));
+		pQuitButton->VSetSize(cVector2(pSinglePlayerButton->VGetWidth(), pSinglePlayerButton->VGetHeight()));
 		function<void (bool)> callbackQuitBtn;
 		callbackQuitBtn = bind(&cGame::QuitButtonPressed, pGame, _1);
 		pQuitButton->VRegisterCallBack(callbackQuitBtn);
@@ -159,9 +171,9 @@ void cStateMenuScreen::VOnUpdate(cGame *pGame)
 
 void cStateMenuScreen::VOnExit(cGame *pGame)
 {
-	if (pGame->m_pHumanView->m_pParentControl != NULL)
+	if (pGame->m_pHumanView->m_pAppWindowControl != NULL)
 	{
-		pGame->m_pHumanView->m_pParentControl->VRemoveAllChildren();
+		pGame->m_pHumanView->m_pAppWindowControl->VRemoveAllChildren();
 	}
 
 	pGame->m_pSound->RemoveSound(pGame->GS_MAIN_MENU_MUSIC);
@@ -195,10 +207,9 @@ void cStatePlayGame::VOnEnter(cGame *pGame)
 {
 	shared_ptr<ISprite> pTableSprite = ISprite::CreateSprite();
 
-	pTableSprite->Init("Sprites\\Table.jpg");
-	pTableSprite->SetSize((float)pGame->m_iDisplayWidth, (float)pGame->m_iDisplayHeight);
-	pTableSprite->SetPosition(cVector3(0.0f, 0.0f, 0.0f));
-	pTableSprite->SetFlags(D3DXSPRITE_ALPHABLEND);
+	pTableSprite->VInitialize("Sprites\\Table.jpg");
+	pTableSprite->VSetSize(cVector2((float)pGame->m_iDisplayWidth, (float)pGame->m_iDisplayHeight));
+	pTableSprite->VSetPosition(cVector2(0.0f, 0.0f));
 	pGame->m_pHumanView->PushElement(pTableSprite);
 
 	cPongGameElement::SetTableHeight(pGame->m_iDisplayHeight);
@@ -209,40 +220,35 @@ void cStatePlayGame::VOnEnter(cGame *pGame)
 	pGame->m_pGameElements[pGame->PGE_PADDLE_LEFT] = DEBUG_NEW cPaddle();
 	pGame->m_pGameElements[pGame->PGE_PADDLE_LEFT]->Init(cVector3(10.0f, (float)pGame->m_iDisplayHeight/2, 0.0f), "Sprites\\paddle.jpg");
 	pSprite = const_pointer_cast<ISprite>(pGame->m_pGameElements[pGame->PGE_PADDLE_LEFT]->GetSprite());
-	pSprite->SetFlags(D3DXSPRITE_ALPHABLEND);
 	pGame->m_pHumanView->PushElement(pSprite);
 
 	pGame->m_pGameElements[pGame->PGE_PADDLE_RIGHT] = DEBUG_NEW cPaddle();
 	pGame->m_pGameElements[pGame->PGE_PADDLE_RIGHT]->Init(cVector3((float)(pGame->m_iDisplayWidth), (float)pGame->m_iDisplayHeight/2, 0.0f), "Sprites\\paddle.jpg");
 	pSprite = const_pointer_cast<ISprite>(pGame->m_pGameElements[pGame->PGE_PADDLE_RIGHT]->GetSprite());
-	pSprite->SetFlags(D3DXSPRITE_ALPHABLEND);
 	pGame->m_pHumanView->PushElement(pSprite);
 
 	pGame->m_pGameElements[pGame->PGE_WALL_UP] = DEBUG_NEW cWall();
 	pGame->m_pGameElements[pGame->PGE_WALL_UP]->Init(cVector3(0.0f, 0.0f, 0.0f), "Sprites\\wall.png");
 	pSprite = const_pointer_cast<ISprite>(pGame->m_pGameElements[pGame->PGE_WALL_UP]->GetSprite());
-	pSprite->SetFlags(D3DXSPRITE_ALPHABLEND);
 	pGame->m_pHumanView->PushElement(pSprite);
 
 	pGame->m_pGameElements[pGame->PGE_WALL_DOWN] = DEBUG_NEW cWall();
 	pGame->m_pGameElements[pGame->PGE_WALL_DOWN]->Init(cVector3(0.0f, (float)pGame->m_iDisplayHeight, 0.0f), "Sprites\\wall.png");
 	pSprite = const_pointer_cast<ISprite>(pGame->m_pGameElements[pGame->PGE_WALL_DOWN]->GetSprite());
-	pSprite->SetFlags(D3DXSPRITE_ALPHABLEND);
 	pGame->m_pHumanView->PushElement(pSprite);
 
 	pGame->m_pGameElements[pGame->PGE_BALL] = DEBUG_NEW cBall();
 	pGame->m_pGameElements[pGame->PGE_BALL]->Init(cVector3((float)pGame->m_iDisplayWidth/2, (float)pGame->m_iDisplayHeight/2, 0.0f), "Sprites\\ball.png");
 	pSprite = const_pointer_cast<ISprite>(pGame->m_pGameElements[pGame->PGE_BALL]->GetSprite());
-	pSprite->SetFlags(D3DXSPRITE_ALPHABLEND);
 	pGame->m_pHumanView->PushElement(pSprite);
 
 	pGame->m_pScore = DEBUG_NEW cScore[2]();
 	
 	pGame->m_pScore[0].Init(D3DXVECTOR3(0.0f, 0.0f, 0.0f));
-	pGame->m_pHumanView->PushElement(pGame->m_pScore[0].GetFont());
+	pGame->m_pHumanView->m_pAppWindowControl->VAddChildControl(pGame->m_pScore[0].GetLabel());
 	
 	pGame->m_pScore[1].Init(D3DXVECTOR3((float)pGame->m_iDisplayWidth, 0.0f, 0.0f));
-	pGame->m_pHumanView->PushElement(pGame->m_pScore[1].GetFont());
+	pGame->m_pHumanView->m_pAppWindowControl->VAddChildControl(pGame->m_pScore[1].GetLabel());
 	
 	pGame->m_pSound->CreateSound(pGame->GS_BALL_WALL_COLLISION, "resources\\Sounds\\SFX\\collision1.wav");
 	pGame->m_pSound->CreateSound(pGame->GS_BALL_PADDLE_COLLISION, "resources\\Sounds\\SFX\\collision2.wav");
