4fd80446e6863711bac99635193e0b3981f05ae5
ankur.sheel@gmail.com <ankur.sheel@gmail.com@c580c124-fe00-11dd-b556-9305e745af9e>
2013-02-22 06:33:59 +0000

    Updated by AS 2) added projects and solution for vs2010
    git-svn-id: https://sppong.googlecode.com/svn/trunk@344 c580c124-fe00-11dd-b556-9305e745af9e

--- Source/Base/Includes/Vector2.h
+++ Source/Base/Includes/Vector2.h
@@ -12,115 +12,137 @@
 
 namespace Base
 {
-/********************************************//**
- * @brief Vector2 Struct.
- *
- ***********************************************/
-class cVector2
+	///////////////////////////////////////////////////////////////////////////
+	/// @brief Encapsulates a 2D vector with X and Y components .
+	///
+	///////////////////////////////////////////////////////////////////////////
+	class cVector2
 	{
 	public:
+		///////////////////////////////////////////////////////////////////////
+		/// Constructor
+		///
+		///
+		///////////////////////////////////////////////////////////////////////
 		cVector2();
-		cVector2(const float fX, const float fY);
-		
-		/********************************************//**
-		 * @return True if the vector is 0
-		 *
-		 * returns true if the vector is 0
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Constructor
+		///
+		/// @param[in] X The X component of the vector
+		/// @param[in] Y The Y component of the vector
+		///
+		///////////////////////////////////////////////////////////////////////
+		cVector2(const float X, const float Y);
+		///////////////////////////////////////////////////////////////////////
+		/// Returns true if the vector is 0
+		/// 
+		/// @return True if the vector is 0
+		///
+		///////////////////////////////////////////////////////////////////////
 		bool IsZero() const;	
-		/********************************************//**
-		 * @return Length of the vector
-		 *
-		 * Returns the length of the vector
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Gets the length of the vector
+		///
+		/// @return Length of the vector
+		///
+		///////////////////////////////////////////////////////////////////////
 		float Length() const;
-		/********************************************//**
-		 * @return Squared length of the vector 
-		 *
-		 * returns the squared length of the vector
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Gets the squared length of the vector
+		///
+		/// @return Squared length of the vector 
+		///
+		///////////////////////////////////////////////////////////////////////
 		float LengthSquared() const;
-		/********************************************//**
-		 * 
-		 *
-		 * Converts the Vector into a unit vector
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Converts the Vector into a unit vector
+		///
+		///
+		///////////////////////////////////////////////////////////////////////
 		void Normalize();
-		/********************************************//**
-		 * @param[in] inVec The vector with which the dot product needs to be calculated
-		 * @return The dot product between this vector and the one passed as a parameter
-		 *
-		 * Calculates and returns the dot product
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Calculates and returns the dot product
+		/// 
+		/// @param[in] inVec The vector with which the dot product needs to be 
+		/// calculated
+		/// @return The dot product between this vector and the one passed as a
+		/// parameter
+		/// 
+		///////////////////////////////////////////////////////////////////////
 		float Dot(const cVector2 & inVec) const;
-		/********************************************//**
-		 * @return vector perpendicular to this one along the X axis.
-		 *
-		 * Returns the vector that is perpendicular to this one along the X axis.
-		 ***********************************************/
-		cVector2  PerpendicularAboutXAxis() const;
-		/********************************************//**
-		 * @return vector perpendicular to this one along the Y axis.
-		 *
-		 * Returns the vector that is perpendicular to this one along the Y axis.
-		 ***********************************************/
-		cVector2  PerpendicularAboutYAxis() const;
-		/********************************************//**
-		 * @param[in] dMax The maximum length of the vector
-		 *
-		 * Adjusts x and y so that the length of the vector does not exceed dMax
-		 ***********************************************/
-		void Truncate(const float fMax);
-		/********************************************//**
-		 * @param[in] inVec The vector with which the distance needs to be calculated
-		 * @return Euclidean distance 
-		 *
-		 * returns the distance between this vector and the one passed as a parameter
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Get the perpendicular vector about the X axis.
+		///
+		/// @return perpendicular vector about the X axis.
+		///
+		///////////////////////////////////////////////////////////////////////
+		cVector2 PerpendicularAboutXAxis() const;
+		///////////////////////////////////////////////////////////////////////
+		/// Get the perpendicular vector about the Y axis.
+		///
+		/// @return perpendicular vector about the Y axis.
+		///
+		///////////////////////////////////////////////////////////////////////
+		cVector2 PerpendicularAboutYAxis() const;
+		///////////////////////////////////////////////////////////////////////
+		/// Gets the distance between this and the passed in vector
+		///
+		/// @param[in] inVec The vector with which the distance needs to be
+		/// calculated
+		/// @return Euclidean distance 
+		///
+		///////////////////////////////////////////////////////////////////////
 		float Distance(const cVector2 & inVec) const;
-		/********************************************//**
-		  * @param[in] inVec The vector with which the distance needs to be calculated
-		  * @return Squared euclidean distance 
-		  *
-		  * returns the squared distance between this vector and the one passed as a parameter
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Gets the squared distance between this and the passed in vector
+		///
+		/// @param[in] inVec The vector with which the squared distance needs
+		/// to be calculated
+		/// @return Squared Euclidean distance 
+		///
+		///////////////////////////////////////////////////////////////////////
 		float DistanceSquared(const cVector2 & inVec) const;
-		/********************************************//**
-		 * @param[in] const cVector2 & norm
-		 * @return 
-		 *
-		 * Reflects the normalized vector it is operating upon.
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Reflects the normalized vector it is operating upon.
+		///
+		/// @param[in] norm The normalized vector
+		///
+		///////////////////////////////////////////////////////////////////////
 		void Reflect(const cVector2 & norm);
-		/********************************************//**
-		 * @return Reverse of this vector
-		 *
-		 * returns the vector that is the reverse of this vector
-		 ***********************************************/
-		cVector2  GetReverse() const;
-		/********************************************//**
-		  *
-		  * Sets the components to their absolute values
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Gets the reverse vector
+		///
+		/// @return Reverse of this vector
+		///		 
+		///////////////////////////////////////////////////////////////////////
+		cVector2 GetReverse() const;
+		///////////////////////////////////////////////////////////////////////
+		/// Sets X and Y to their absolute values
+		///
+		///
+		///////////////////////////////////////////////////////////////////////
 		void AbsTo();
-		/********************************************//**
-		  * @return A signed, unit vector containing only the largest coordinate
-		  *
-		  * Get the largest coordinate and return a signed, unit vector
-		  * containing only that coordinate
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Get the largest component and return a signed, unit vector
+		/// containing only that component
+		///
+		/// @return A signed, unit vector containing only the largest component
+		///
+		///////////////////////////////////////////////////////////////////////
 		cVector2 MajorAxis() const;
-		/********************************************//**
-		  * @return A signed, unit vector containing only the smallest coordinate
-		  *
-		  * Get the smallest coordinate and return a signed, unit vector
-		  * containing only that coordinate
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Get the smallest component and return a signed, unit vector
+		/// containing only that component
+		///
+		/// @return A signed, unit vector containing only the largest component
+		///
+		///////////////////////////////////////////////////////////////////////
 		cVector2 MinorAxis() const;
-		/********************************************//**
-		  *
-		  * Changes the sign of the components
-		 ***********************************************/
+		///////////////////////////////////////////////////////////////////////
+		/// Changes the sign of the components
+		///
+		///
+		///////////////////////////////////////////////////////////////////////
 		void NegTo();
 
 		const cVector2 & operator+=(const cVector2 & inVec);
@@ -134,18 +156,19 @@ class cVector2
 		const float operator[](const unsigned int i) const;
 
 		static cVector2 Zero();
+
 	public:
 		float	x;	/*!< X component of Vector2 */
 		float	y;	/*!< Y component of Vector2 */
 	};
 
-inline cVector2 operator*(const cVector2 & inVec1, const float fVal);
-inline cVector2 operator*(const float fVal, const cVector2 & inVec1);
-inline cVector2 operator/(const cVector2 & inVec1, const float fVal);
-inline cVector2 operator-(const cVector2 & inVec1, const cVector2 & inVec2);
-inline cVector2 operator+(const cVector2 & inVec1, const cVector2 & inVec2);
-inline cVector2 operator*(const cVector2 & inVec1, const cVector2 & inVec2);
+	inline cVector2 operator*(const cVector2 & inVec1, const float fVal);
+	inline cVector2 operator*(const float fVal, const cVector2 & inVec1);
+	inline cVector2 operator/(const cVector2 & inVec1, const float fVal);
+	inline cVector2 operator-(const cVector2 & inVec1, const cVector2 & inVec2);
+	inline cVector2 operator+(const cVector2 & inVec1, const cVector2 & inVec2);
+	inline cVector2 operator*(const cVector2 & inVec1, const cVector2 & inVec2);
 
-#include "Vector2.inl"
+	#include "Vector2.inl"
 }
 #endif // Vector2_h__
